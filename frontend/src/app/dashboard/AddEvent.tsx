f16a6530c3d5cf18daec69b71f9a86a36996ee07
shadwar123 <shadwarnayyar872@gmail.com>
2025-02-27 15:56:38 +0530

    session manage

--- frontend/src/app/dashboard/AddEvent.tsx
+++ frontend/src/app/dashboard/AddEvent.tsx
@@ -92,7 +92,6 @@ export const AddEvent: React.FC<AddEventProps> = ({
         if (resp.error) {
           throw resp;
         }
-        await listUpcomingEvents();
         const { access_token, expires_in } = (
           window as any
         ).gapi.client.getToken();
@@ -143,98 +142,6 @@ export const AddEvent: React.FC<AddEventProps> = ({
     setIsApiLoaded(false); // Optionally reset API loaded state
     window.location.reload();
   }
-
-  async function listUpcomingEvents() {
-    if (!isApiLoaded) {
-      console.error("Google Calendar API not loaded");
-      return;
-    }
-
-    try {
-      const request = {
-        calendarId: "primary",
-        timeMin: new Date().toISOString(),
-        showDeleted: false,
-        singleEvents: true,
-        maxResults: 10,
-        orderBy: "startTime",
-      };
-      const response = await (window as any).gapi.client.calendar.events.list(
-        request
-      );
-      const events = response.result.items;
-
-      if (!events || events.length === 0) {
-        console.log("No events found.");
-        return;
-      }
-
-      const output = events.reduce(
-        (str: string, event: any) =>
-          `${str}${event.summary} (${
-            event.start.dateTime || event.start.date
-          })\n`,
-        "Events:\n"
-      );
-      console.log(output);
-    } catch (err: any) {
-      console.error("Error listing events:", err.message);
-    }
-  }
-
-  // async function addMultipleEvents() {
-  //   if (!isApiLoaded) {
-  //     console.error("Google Calendar API not loaded");
-  //     return;
-  //   }
-
-  //   try {
-  //     for (let i = 0; i < eventListData.length; i++) {
-  //       const eventDate = new Date();
-  //       eventDate.setDate(eventDate.getDate() + i);
-
-  //       // Create a new Date object for the event start time
-  //       const [hours, minutes] = selectedTime.split(":").map(Number);
-  //       const eventStartTime = new Date(eventDate);
-  //       eventStartTime.setHours(hours, minutes);
-
-  //       // Create an end time that is one hour after the start time
-  //       const eventEndTime = new Date(eventStartTime);
-  //       eventEndTime.setHours(eventEndTime.getHours() + 1);
-  //       console.log("time",eventStartTime)
-  //       const event = {
-  //         kind: "calendar#event",
-  //         summary: `Day ${i + 1}`,
-  //         location: `${playlistUrl}`,
-  //         description: eventListData[i],
-  //         start: {
-  //           dateTime: eventStartTime.toISOString(),
-  //           timeZone: "UTC",
-  //         },
-  //         end: {
-  //           dateTime: eventEndTime.toISOString(),
-  //           timeZone: "UTC",
-  //         },
-  //         reminders: {
-  //           useDefault: true,
-  //         },
-  //         guestsCanSeeOtherGuests: true,
-  //       };
-
-  //       const response = await (
-  //         window as any
-  //       ).gapi.client.calendar.events.insert({
-  //         calendarId: "primary",
-  //         resource: event,
-  //         sendUpdates: "all",
-  //       });
-
-  //       console.log(`Event ${i + 1} added:`, response);
-  //     }
-  //   } catch (error) {
-  //     console.error("Error adding events:", error);
-  //   }
-  // }
 
 
 
