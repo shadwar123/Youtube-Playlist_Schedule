74bfae0d9807cc7d2272d862852a5a6967de6f93
shadwar123 <shadwarnayyar872@gmail.com>
2025-03-18 13:53:02 +0530

    cron jobs

---
+++ server/src/config/cron.ts
@@ -0,0 +1,19 @@
+import { CronJob } from "cron";
+import axios from "axios";
+
+const PING_URL = "https://full-stack-server-j2k9.onrender.com";
+
+export const job = new CronJob(
+  "*/14 * * * *", // Runs every 14 minutes
+  async function () {
+    try {
+      const response = await axios.get(PING_URL);
+      console.log(`✅ Server Pinged at ${new Date().toLocaleTimeString()}`);
+    } catch (error:any) {
+      console.error(`❌ Failed to ping server:`, error.message);
+    }
+  },
+  null, // onComplete
+  false, // Do not start immediately
+  "UTC" // Change if you need a different time zone
+);
